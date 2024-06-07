5b8de17fd4fbdaf99f8cc9f4d745b411fbeab9c3
Pita <134651891+pitashaw@users.noreply.github.com>
2024-06-07 13:03:56 +1200

    Add files via upload

--- 00_FC_Base_v1.py
+++ 00_FC_Base_v1.py
@@ -20,13 +20,25 @@ def num_check(question, error, num_type):
         except ValueError:
             print(error)
 
-# Main routine goes here
-get_int = num_check("How many do you need? ",
-                    "Please enter an amount more than 0\n",
-                    int)
-get_cost = num_check("How much does it cost? $"
-                     "Please enter a number more than 0\n",
-                     float)
 
-print("You need: {}".format(get_int))
-print("It costs: ${}".format(get_cost))
+
+# Checks that user has entered yes / no to a question
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
+
+
+# Main routine goes here
