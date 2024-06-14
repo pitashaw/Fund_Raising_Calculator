3e908ce88718714d564f449d3e0e447790987c52
Pita <134651891+pitashaw@users.noreply.github.com>
2024-06-14 12:55:32 +1200

    Add files via upload

--- 00_FC_Base_v2.py
+++ 00_FC_Base_v2.py
@@ -125,6 +125,72 @@ def expense_print(heading, frame, subtotal):
     print("{} Costs: ${:.2f}".format(heading, subtotal))
     return ""
 
+# work out profit goal and total sales required
+def profit_goal(total_costs) :
+
+    # Initialise variables and error message
+    error = "Please enter a valid profit goal\n"
+
+    valid = False
+    while not valid:
+
+        # ask for profit goal...
+        response = input("What is your profit goal (eg $500 or 50%) ")
+
+        # check if first character is $...
+        if response[0] == "$":
+            profit_type = "$"
+            # Get amount (everything after the $)
+            amount = response[1:]
+
+        # check if last character is %
+        elif response [-1] == "%":
+            profit_type = "%"
+            # Get amount (everything before the %)
+            amount = response[:-1]
+
+        else:
+            # set response to amount for now
+            profit_type = "unknown"
+            amount = response
+
+        try:
+            # Check amount is a number more than zero...
+            amount = float(amount)
+            if amount <= 0:
+                print (error)
+                continue
+
+        except ValueError:
+            print(error)
+            continue
+
+        if profit_type == "unkown" and amount >= 100:
+            dollar_type = yes_no("Do you mean ${:.2f}. "
+                                 "ie {:.2f} dollars? ,"
+                                 "y / n".format(amount, amount))
+
+            # Set profit type based on user answer above
+            if dollar_type == "yes":
+                profit_type = "$"
+            else:
+                profit_type = "%"
+
+        elif profit_type == "unknown" and amount < 100:
+            percent_type = yes_no("Do you ,mean {}%? , "
+                                  "y / n".format(amount))
+            if percent_type == "yes":
+                profit_type = "%"
+            else:
+                profit_type = "$"
+
+        # return profit goal to main routine
+        if profit_type == "$":
+            return amount
+        else:
+            goal = (amount / 100) * total_costs
+            return goal
+
 # *** Main routine starts here ***
 
 # Get product name
@@ -149,10 +215,13 @@ else:
     fixed_sub = 0
 
 # Find Total Costs
+all_costs = variable_sub + fixed_sub
+profit_target = profit_goal(all_costs)
 
 # Ask user for profit goal
 
 # Calculate recommended price
+selling_price = 0
 
 # Write data to file
 
@@ -165,3 +234,16 @@ expense_print("Variable", variable_frame, variable_sub)
 
 if have_fixed == "yes":
     expense_print("Fixed", fixed_frame[['Cost']], fixed_sub)
+
+print()
+print("**** Total Costs: ${:.2f} ****".format(all_costs))
+print()
+
+print()
+print("**** Profit & Sales Targets ****")
+print("Profit Target: ${:.2f}".format(profit_target))
+print("Total Sales: ${:.2f}".format(all_costs + profit_target))
+
+print()
+print("**** Recommended Selling Price:"
+      " ${:.2f}".format(selling_price))
